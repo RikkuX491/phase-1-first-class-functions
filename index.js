b6e3e76201eca8061b01ba7fcbc0b65d0fab56d1
Ricardo Guerra <RikkuG491@gmail.com>
2024-01-30 16:17:12 -0500

    Added solution code

---
+++ index.js
@@ -0,0 +1,18 @@
+// Answer to instruction # 1
+function receivesAFunction(callbackFunction){
+    callbackFunction()
+}
+
+// Answer to instruction # 2
+function returnsANamedFunction(){
+    return function fn(){
+
+    }
+}
+
+// Answer to instruction # 3
+function returnsAnAnonymousFunction(){
+    return function (){
+
+    }
+}
