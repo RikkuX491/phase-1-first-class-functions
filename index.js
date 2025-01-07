685e21145f50f7c3fbff830e41e4fb5381c0f190
Ricardo Guerra <RikkuG491@gmail.com>
2025-01-07 15:24:27 -0500

    Added solution code

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(getAnimal){
+    getAnimal()
+}
+
+function returnsANamedFunction(){
+    return function pizza(){}
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){}   
+}
